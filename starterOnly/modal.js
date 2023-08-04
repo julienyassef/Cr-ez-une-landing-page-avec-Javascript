8028d09f5eb1c619ab6ba7e29069423f7d7b0841
julien yassef <juyassef@gmail.com>
2023-08-04 15:55:46 +0200

    ajout todo fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalcrossclose = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+};
+
+// close modal event
+modalcrossclose.forEach((btn) => btn.addEventListener("click", closeModal));
+
+// close modal event
+function closeModal() {
+  modalbg.style.display = "none";
 }
-
-
